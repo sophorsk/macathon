18189a8669eb8f7b1c657cfa81e3cea48bad01c0
sophorsk <khut.sophors@gmail.com>
2014-02-28 21:29:38 -0600

    Added some files

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express');
-var http = require('http');
 var app = express();
+var MemoryStore = require('connect').session.MemoryStore;
 
-app.use(express.bodyParser());
-app.use(express.cookieParser());
+app.configure(function() {
+    app.set('view engine', 'jade');
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.limit('1mb'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({
+        secret: "PartyFinder secret key",
+        store: new MemoryStore()
+    }));
+});
 
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Server listening on port ' + app.get('port'));
-})
+app.get('/', function(req, res) {
+    res.render('index.jade');
+});
+
+app.listen(8080);
+console.log("Application is at localhost:8080")
