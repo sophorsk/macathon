ba7da08a461fc7c52ae20f9a58be8f207e9890e1
sophorsk <khut.sophors@gmail.com>
2014-02-28 23:38:22 -0600

    updated

--- public/js/views/profile.js
+++ public/js/views/profile.js
@@ -1,7 +1,20 @@
-/**
- * Created with JetBrains WebStorm.
- * User: sophorskhut
- * Date: 2/28/14
- * Time: 11:23 PM
- * To change this template use File | Settings | File Templates.
- */
+define(['CoBoView', 'text!templates/profile.html'],
+    function(CoBoView, profileTemplate) {
+        var profileView = CoBoView.extend({
+            el: $('#content'),
+
+            initialize: function() {
+
+            },
+
+            render: function() {
+
+                this.$el.html(
+                    _.template(profileTemplate, this.model.toJSON())
+                );
+
+            }
+        });
+
+        return profileView;
+});
