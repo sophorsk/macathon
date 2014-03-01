d20df2862669fc36a8d46f3502f02f7aebd15acf
Eric Biggers <ebiggers3@gmail.com>
2014-03-01 02:25:58 -0600

    Fix login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var MemoryStore = require('connect').session.MemoryStore;
 var pg = require('pg').native;
+var sha1 = require('sha1');
 
 // import the models
 var models = {
@@ -34,23 +35,25 @@ app.get('/account/authenticated', function(req, res) {
 
 app.post('/login', function(req, res) {
     console.log('login request');
-    var email = req.param('email', null);
-    var password = req.param('password', null);
+    var email = req.param('email');
+    var password = req.param('password');
 
     if ( email == null || email.length < 1 || password == null || password.length < 1 ) {
         res.send(400);
         return;
     }
 
-    models.Model.login(email, password, function(err, account) {
-        if (!account) {
+    var password_hash = sha1(password);
+
+    models.Model.login(email, password_hash, function(err, account) {
+        if (err) {
             res.send(400);
             return;
         }
 
         console.log('login was successful');
         req.session.loggedIn = true;
-        req.session.accountId = account;
+        req.session.accountId = account.id;
         res.send(200);
     });
 
