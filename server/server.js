5aecba149f1ff1444d58d62189f210e2ce8fc3e0
sophorsk <khut.sophors@gmail.com>
2014-02-28 19:26:45 -0600

    Commit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-app.js
+var express = require("express");
+var app = express();
+
+var models = {
+	Model: require('./models/Model')
+};
+
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+});
+
