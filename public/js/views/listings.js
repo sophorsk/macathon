f91f6d754813140d16175fbcc697b5d5a951e77f
Tony <pbach@macalester.edu>
2014-08-06 13:55:49 -0500

    Made the sell pop-up work from Your Account page

--- public/js/views/listings.js
+++ public/js/views/listings.js
@@ -4,27 +4,41 @@ define(['CoBoView', 'text!templates/listings.html', 'views/item'],
         var listingsView = CoBoView.extend({
 
             el: 'body',
-            /*
+            
             events: {
-                "click .post_submit": "postItem"
-            },*/
-            /*
-            postItem: function() {
+                "click .sell_item": "sellItem",
+                // "click .search_item": "searchItems"
+            },
+            
+            sellItem: function() {
                 var $messageArea = this.$('.messageArea');
-
                 $.post('/api/post_item', {
                     name: $('input[name=name]').val(),
                     category: $('select.category option:selected').val(),
                     description: $('input[name=description]').val(),
                     price_in_cents: $('input[name=price_in_cents]').val()
                 }, function(data) {
-                    console.log(data);
                     $messageArea.text('Your item has been posted!');
                 }).error(function() {
-                        $messageArea.text('Your item cannot be posted!');
-                    });
+                    $messageArea.text('Your item cannot be posted!');
+                });
                 return false;
-            },*/
+            },
+
+            // searchItems: function() {
+            //     var view = this;
+            //     $.get("/api/accounts/me/items", {
+            //             category: $('select.category option:selected').val(),
+            //             q: $('input[name=searchString]').val()
+            //         },
+            //         function(data) {
+            //             view.render(data);
+            //         }).error(function() {
+            //             $('.your_items').text('No items found !');
+            //             $('.your_items').slideDown();
+            //         });
+            //     return false;
+            // },
 
             getData: function(url) {
                 var model = null;
